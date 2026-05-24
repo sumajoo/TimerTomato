57b72bc7f476f69ef8dc87d7946434dbab8007ec
Jonas <41197679+sumajoo@users.noreply.github.com>
2026-05-24 12:36:55 +0200

    Initial Commit

---
+++ TimerTomatoTests/TimerTomatoTests.swift
@@ -0,0 +1,19 @@
+//
+//  TimerTomatoTests.swift
+//  TimerTomatoTests
+//
+//  Created by Jonas Becker on 24.05.26.
+//
+
+import Testing
+@testable import TimerTomato
+
+struct TimerTomatoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
